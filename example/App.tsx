9f6840a28fec40da5b0fd9b6a7fa96468793dd85
Kelvin Chu <chubillkelvin@gmail.com>
2020-01-20 14:00:53 +0800

    Update example app.

--- example/App.tsx
+++ example/App.tsx
@@ -10,20 +10,12 @@
 
 import React from "react";
 import {SafeAreaView, StyleSheet, View, Text} from "react-native";
+import {Colors} from "./Colors";
 import AutoScrollFlatList from "react-native-autoscroll-flatlist";
-
-enum Colors {
-    "#e04836",
-    "#f39d41",
-    "#8d5924",
-    "#5696bc",
-    "#2f5168",
-}
 
 interface Message {
     id: string;
-    content: string;
-    color: string;
+    isSelf: boolean;
 }
 
 interface Props {}
@@ -49,10 +41,7 @@ export default class App extends React.PureComponent<Props, State> {
             const newMessages = [...this.state.messages];
             newMessages.push({
                 id: newMessages.length.toString(),
-                content: Math.random()
-                    .toString(36)
-                    .slice(-5),
-                color: Colors[Math.floor(Math.random() * Object.keys(Colors).length)],
+                isSelf: Math.random() < 0.5, // 50% chance the new message will be from self
             });
             this.setState({messages: newMessages});
         }, 1000);
@@ -62,13 +51,24 @@ export default class App extends React.PureComponent<Props, State> {
         clearInterval(this.timer!);
     }
 
+    renderMessage = ({item}: {item: Message}) => {
+        return (
+            <View style={[styles.messageContainer, item.isSelf && styles.messageSelf]}>
+                <Text>{item.isSelf ? "Me" : "Someone"}</Text>
+                <View style={styles.grayContent} />
+                <View style={styles.grayContent} />
+                <View style={styles.grayContent} />
+            </View>
+        );
+    };
+
     render() {
         return (
             <SafeAreaView style={styles.container}>
                 <View style={styles.title}>
                     <Text style={styles.titleText}>react-native-autoscroll-flatlist</Text>
                 </View>
-                <AutoScrollFlatList ref={this.ref} data={this.state.messages} renderItem={({item}) => <Text style={[styles.message, {color: item.color}]}>{item.content}</Text>} keyExtractor={item => item.id} style={styles.flatList} />
+                <AutoScrollFlatList ref={this.ref} data={this.state.messages} renderItem={this.renderMessage} keyExtractor={item => item.id} style={styles.flatList} />
             </SafeAreaView>
         );
     }
@@ -77,31 +77,39 @@ export default class App extends React.PureComponent<Props, State> {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#2f5168",
+        backgroundColor: Colors.TEAL_GREEN,
     },
     title: {
-        height: 40,
+        height: 50,
+        justifyContent: "center",
     },
     titleText: {
         fontSize: 24,
         textAlign: "center",
-        color: "#e04836",
+        color: Colors.WHITE,
         fontWeight: "bold",
     },
     flatList: {
-        backgroundColor: "#ffffff",
+        backgroundColor: Colors.PALE_GRAY,
     },
-    message: {
-        fontSize: 24,
+    messageContainer: {
+        borderRadius: 5,
+        backgroundColor: Colors.WHITE,
+        width: "80%",
+        minHeight: 65,
+        margin: 8,
+        padding: 8,
+        justifyContent: "space-between",
     },
-    scrollToEndButton: {
-        height: 60,
+    messageSelf: {
+        alignSelf: "flex-end",
+        backgroundColor: Colors.LIGHT_BLUE,
+        alignItems: "flex-end",
+    },
+    grayContent: {
+        height: 6,
         width: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#e04836",
-    },
-    buttonText: {
-        fontSize: 24,
+        backgroundColor: Colors.GAINSBORO,
+        borderRadius: 10,
     },
 });
---
+++ example/Colors.ts
@@ -0,0 +1,8 @@
+export enum Colors {
+    TEAL_GREEN = "#006d5b",
+    WHITE = "#ffffff",
+    LIGHT_BLUE = "#add8e6",
+    PALE_GRAY = "#f1f1f1",
+    GAINSBORO = "#dcdcdc",
+    BLACK = "#000000",
+}
