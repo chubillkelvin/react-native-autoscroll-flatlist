18d5a455aaf220eaf72ce43f9edf3308a802ced3
Kelvin Chu <chubillkelvin@gmail.com>
2020-10-04 14:21:52 +0800

    Update gitignore and build script to account for change in example project.

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -1,10 +1,13 @@
 const fs = require("fs");
 const fsExtra = require("fs-extra");
 const spawn = require("./spawn");
+const package = require("../package.json");
 
 function build() {
     fsExtra.emptyDirSync("../build/dist");
-    fs.copyFileSync("./package.json", "../build/dist/package.json", fs.constants.COPYFILE_FICLONE);
+    package["main"] = "index.js";
+    fs.writeFileSync("../build/dist/package.json", JSON.stringify(package));
+    spawn("prettier", ["--config", "./prettier.json", "--write", "../build/dist/package.json"]);
     fs.copyFileSync("../README.md", "../build/dist/README.md", fs.constants.COPYFILE_FICLONE);
     return spawn("tsc", ["-p", "./tsconfig.json"]);
 }
