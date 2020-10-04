70c1afeb0f1c4099bf13dce8d49bd8559d8e6607
Kelvin Chu <chubillkelvin@gmail.com>
2020-10-04 14:21:36 +0800

    Update metro config for demo project to take local dependency correctly on app.

---
+++ example/expo/metro.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+    projectRoot: path.resolve(__dirname),
+    watchFolders: [path.resolve(__dirname, "../../src/node_modules"), path.resolve(__dirname, "../../src")],
+    resolver: {
+        extraNodeModules: new Proxy(
+            {},
+            {
+                get: (target, name) => {
+                    if (name === "react-native-autoscroll-flatlist") {
+                        return path.join(__dirname, `../../src/`);
+                    }
+                },
+            }
+        ),
+    },
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true,
+            },
+        }),
+    },
+};
