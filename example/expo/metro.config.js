8a8153e1aedc09df0543b57b64b5caa7b021e416
Kelvin Chu <chubillkelvin@gmail.com>
2020-10-05 10:52:06 +0800

    v1.9.1: Refactor dependencies, update example metro config.

--- example/expo/metro.config.js
+++ example/expo/metro.config.js
@@ -2,14 +2,18 @@ const path = require("path");
 
 module.exports = {
     projectRoot: path.resolve(__dirname),
-    watchFolders: [path.resolve(__dirname, "../../src/node_modules"), path.resolve(__dirname, "../../src")],
+    watchFolders: [path.resolve(__dirname, "./node_modules"), path.resolve(__dirname, "../../src/node_modules"), path.resolve(__dirname, "../../src")],
     resolver: {
         extraNodeModules: new Proxy(
             {},
             {
                 get: (target, name) => {
                     if (name === "react-native-autoscroll-flatlist") {
-                        return path.join(__dirname, `../../src/`);
+                        return path.join(__dirname, "../../src/");
+                    } else if (name === "react") {
+                        return path.join(__dirname, "./node_modules/react");
+                    } else if (name === "react-native") {
+                        return path.join(__dirname, "./node_modules/react-native");
                     }
                 },
             }
